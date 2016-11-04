b1bf4269445a55062ef91ee93379a5694c046e60
Prithi <pprithip@sfu.ca>
2016-11-04 04:11:17 +0000

    script.py : correctly mapping IPI

---
+++ script.py
@@ -0,0 +1,22 @@
+import re
+## creates the two-dimensional array
+arr = []
+for i in range(0,5) : 
+    arr.append([])
+count=0
+## puts the ipi in the first column of the array
+for line in open("peptide_o.txt","r") : 
+    fragment = line.split("\t")
+    arr[0].append(fragment[0])
+    arr[1].append(fragment[1])
+    
+    for line2 in open("mouse_qualitative.txt","r") : 
+        f = line2.split("\t")
+        found = re.match(arr[0][count-1],f[0])
+        if found : 
+            arr[1][count-1] = f[1]## return the description 
+    count=count+1
+c=0
+for x in range(1,330) : 
+    c=c+1
+print c
