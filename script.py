fce571a424089ae1b6a87ee771c0df75bae4914a
PrithiPal <pprithip@sfu.ca>
2016-11-04 04:44:54 +0000

    script.py : binary search added

--- script.py
+++ script.py
@@ -1,5 +1,20 @@
 import re
 ## creates the two-dimensional array
+def binary_search(array, target):
+    lower = 0
+    upper = len(array)
+    while lower < upper:   # use < instead of <=
+        x = lower + (upper - lower) // 2
+        val = array[x]
+        if target == val:
+            return x
+        elif target > val:
+            if lower == x:   # this two are the actual lines
+                break        # you're looking for
+            lower = x
+        elif target < val:
+            upper = x
+
 arr = []
 for i in range(0,5) : 
     arr.append([])
@@ -14,6 +29,9 @@ for line in open("peptide_o.txt","r") :
         f = line2.split("\t")
         found = re.match(arr[0][count-1],f[0])
         if found : 
-            arr[1][count-1] = f[1]## return the description 
-    
-    count=count+1
+            arr[1][count-1] = f[1] ## return the description 
+
+
+for line in open("mouse1_loopstat.txt","r") : 
+    f = line.split("\t")
+    print f[0]
