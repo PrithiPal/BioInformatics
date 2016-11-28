f8caf13e407b9e8c4b3aa10720c856e2446efff8
PrithiPal <pprithip@sfu.ca>
2016-11-28 00:57:20 +0000

    Script.py outputs the correct format for worm.fasta

---
+++ script.py
@@ -0,0 +1,33 @@
+arr = []
+count = 0
+for line in open("short_worm.fasta" ) : 
+    if line[0] == ">" : 
+        
+        fragments = line.split("\t")
+        line = line.replace("\t"," ")
+        initial = fragments[0]
+        fragments[0] = fragments[0].replace(">",">sp|")
+        fragments[0] = fragments[0] + "|" + fragments[1]
+        rep = fragments[0]
+        line = line.replace(fragments[1],"")
+        line = line.replace(initial,rep)
+       # print line
+        arr.append(line)
+        count=count+1
+    else : 
+      #  print line
+        
+        arr.append(line)
+
+
+
+file = open("output.fasta","w")
+
+for i in range(len(arr)-1) : 
+   
+    file.write(arr[i])
+    
+counter=1
+for line in open("output.fasta" ) : 
+    print str(counter) + " line = " + str(len(line))
+    counter=counter+1
