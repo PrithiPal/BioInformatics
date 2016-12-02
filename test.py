aee18ffbe47c2de996d1ddc80907b62582de856e
PrithiPal <pprithip@sfu.ca>
2016-12-02 01:31:45 +0000

    problem with output_fasta count.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 count=0
 for line in open("worm_output.fasta","r") : 
-    if count <=1 : 
+    if count <=2 : 
         if line[0][0] == ">" : 
             count=count+1
         print line + " len = " + str(len(line))
