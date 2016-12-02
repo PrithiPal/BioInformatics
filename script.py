7ee1bf2b41a0595d295183c4f9312b260119e2db
PrithiPal <pprithip@sfu.ca>
2016-12-02 04:24:28 +0000

    function added to script.py

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
-oFileName = raw_input("Please enter the output file name <Identifier> <peptide_seq with hash> : ")
-oFile = open(oFileName,"w")
-
 
 ## credits for this function : Elenasys on stack overflow ##
+
 def insertChar(mystring, position, chartoinsert ): 
     longi = len(mystring)
     mystring   =  mystring[:position] + chartoinsert + mystring[position:] 
@@ -10,23 +8,30 @@ def insertChar(mystring, position, chartoinsert ):
 
 
 ##function to place hash in second position to found sequence, format : N#XS/T
-num_seq=0
-
-for line in open("f1_output.txt","r") : 
-    frag = line.split("\t") ## frag[1] = peptide_seq
-    peptide_seq = frag[1]
+def placeHash(idxFile,outputFile) : 
+    
     num_seq=0
-    for i in range(2,len(peptide_seq)) : 
-        if (peptide_seq[i-2] == "N") and (peptide_seq[i-1]!="P" and (peptide_seq[i] == "S" or peptide_seq[i] == "T")):
-            num_seq=num_seq+1
-            print "seq = " + str(peptide_seq[i-2]) + str(peptide_seq[i-1]) + str(peptide_seq[i]),
-            peptide_seq = insertChar(peptide_seq,i-1,'#')
-            oFile.write(frag[0] +"\t" + peptide_seq)
-            i=i+3
-    if num_seq == 0 : 
-        print "seq not found",
-    print "peptide = " + peptide_seq,
-    print ""    
+    for line in open(idxFile,"r") : 
+        frag = line.split("\t") ## frag[1] = peptide_seq
+        peptide_seq = frag[1]
+        num_seq=0
+        for i in range(2,len(peptide_seq)) : 
+            if (peptide_seq[i-2] == "N") and (peptide_seq[i-1]!="P" and (peptide_seq[i] == "S" or peptide_seq[i] == "T")):
+                num_seq=num_seq+1
+                print "seq = " + str(peptide_seq[i-2]) + str(peptide_seq[i-1]) + str(peptide_seq[i]),
+                peptide_seq = insertChar(peptide_seq,i-1,'#')
+                outputFile.write(frag[0] +"\t" + peptide_seq)
+                i=i+3
+        if num_seq == 0 : 
+            print "seq not found",
+        print "peptide = " + peptide_seq,
+        print ""
     
-    
+    ## main method
+iFileName = raw_input("Please enter the input file name <Identifier> <peptide_seq> : ")
+iFile = open(iFileName,"r")
+oFileName = raw_input("Please enter the output file name <Identifier> <peptide_seq with hash> : ")
+oFile = open(oFileName,"w")
 
+
+placeHash(iFileName,oFile)
