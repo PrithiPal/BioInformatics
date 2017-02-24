157f803a1bb669e3a5dd6345abdb86aeadcee56c
PrithiPal <pprithip@sfu.ca>
2017-02-24 02:57:10 +0000

    insect_not_found function with tests

--- prepare_worm_loopstat_output1.py
+++ prepare_worm_loopstat_output1.py
@@ -7,10 +7,13 @@
 
 # This is reused for the mouse first_loopstat_prepare.py
 import time
+import unittest
 print "starting..."
+import os
+import re
 
 
-    
+global mouse_output_filename   
 ##--------------------------------------------------------------------------------
 
 def main() : 
@@ -20,25 +23,25 @@ def main() :
     MOUSE_DESCRIPTION_INDEX = 1
     MOUSE_PROTEIN_L_INDEX = 2
     
-    
-    
-    
-    mouse_description_filename = 'mouse_human.fasta.txt'
+    mouse_description_filename = 'mouse_qualitative.txt'
     mouse_peptide_filename = 'peptide-v1.txt'
-    mouse_output_filename = 't1[current]-v1.txt'
     mouse_protein_l_filename = 'mouse1_loopstat.txt'
+    mouse_output_filename = 't1[current]-v1.txt'
     
-    prepareEntry(mouse_output_filename,mouse_description_filename,mouse_protein_l_filename,mouse_peptide_filename,MOUSE_DESCRIPTION_INDEX,MOUSE_PROTEIN_L_INDEX)
-
+    prepareWormLoopstatInput1(mouse_output_filename,mouse_description_filename,mouse_protein_l_filename,mouse_peptide_filename,MOUSE_DESCRIPTION_INDEX,MOUSE_PROTEIN_L_INDEX)
+    
+    identify_blank_description(mouse_output_filename)
+    
 
     print "done!!"
     elapsed_time = time.time() - start_time
     print "time elapsed = " + str(elapsed_time)
-
+    
+    return mouse_output_filename
 
 ##--------------------------------------------------------------------------------
 
-def prepareEntry(output_file,description_filename,protein_l_filename,peptide_filename,DESCRIPTION_INDEX,PROTEIN_L_INDEX) : 
+def prepareWormLoopstatInput1(output_file,description_filename,protein_l_filename,peptide_filename,DESCRIPTION_INDEX,PROTEIN_L_INDEX) : 
     arr = []
     for i in range(5) : 
         arr.append([]) ## creates the 5 columns for the data 
@@ -50,20 +53,27 @@ def prepareEntry(output_file,description_filename,protein_l_filename,peptide_fil
     ofile = open(str(output_file),"w")
     
     for lines in open(str(peptide_filename),"r") : 
-    
-        frag1 = lines.split("\t")
-        frag2 = lines.split(" ")
-        identifier = frag1[0]
-        seq_list = frag1[1]
-        total_seq = len(frag1[1].split(" "))-1
+        print lines
         
-        description = findDescription(str(description_filename),identifier,DESCRIPTION_INDEX)
-        protein_length = findProteinLength(str(protein_l_filename,),identifier,PROTEIN_L_INDEX)
+        allowed_line = inspect_notfound(str(lines)) # refer sample variable to the tests below where comment says "allowed line"
         
+        if allowed_line != "0" : 
+            lines = allowed_line
             
-        print_this = str(identifier) + "\t" + str(description) + "\t" + str(protein_length)  + "\t" + str(total_seq) + "\t" + seq_list
-        print print_this
-        ofile.write(print_this)
+            frag1 = lines.split("\t")
+            frag2 = lines.split(" ")
+            
+            identifier = frag1[0]
+            seq_list = frag1[1]
+            total_seq = len(frag1[1].split(" "))-1
+            
+            description = findDescription(str(description_filename),identifier,DESCRIPTION_INDEX)
+            protein_length = findProteinLength(str(protein_l_filename,),identifier,PROTEIN_L_INDEX)
+            
+                
+            print_this = str(identifier) + "\t" + str(description) + "\t" + str(protein_length)  + "\t" + str(total_seq) + "\t" + seq_list
+            #print print_this
+            ofile.write(print_this)
     
 ##--------------------------------------------------------------------------------
     
@@ -100,6 +110,58 @@ def equal(str1, str2) :
         return False
 
 ##--------------------------------------------------------------------------------
+
+def identify_blank_description(filename) : # identifies whether the description is null or empty ("") in file
+    print "Blank Description Test : "
+    response = os.system("awk \'BEGIN{FS = \"\t\"}{if($2 == \"\"){print $0}}\' " + str(filename))
+
+##--------------------------------------------------------------------------------
+
+def inspect_notfound(line) : # inspects the peptide file for not_found lines
+    
+    
+    found_seq = re.split("\t",str(line))
+    identifier = found_seq[0]
+    found_seq = found_seq[1].split(" ")
+    modified_seq = [x for x in found_seq if x != "not_found"]
+    if modified_seq == [''] : 
+        return "0"
+    
+    return identifier + "\t" + ' '.join(modified_seq)
+    
+## -------------------------------------------------------------------------------
+
+class MyTest(unittest.TestCase) : 
+    
+    def test_inspect_notfound_function_partial(self) : 
+        #allowed_line
+        sample = "IPI00113480	NRS(365) not_found "
+        l = inspect_notfound(sample)
+        # print l
+        o = "IPI00113480	NRS(365) "
+        self.assertTrue(l==o)
+        
+    
+    def test_inspect_notfound_function_full(self) : 
+        #allowed_line
+        sample = "IPI00114265	NTT(142) NQS(242) "
+        l = inspect_notfound(sample)
+       # print l
+        o = "IPI00114265	NTT(142) NQS(242) "
+        self.assertTrue(l==o)
+    
+    def test_inspect_notfound_function_none(self) :
+        #not allowed_line
+        sample1 = "IPI00114348	not_found not_found "
+        sample2 = "IPI00122737	not_found "
+        l1 = inspect_notfound(sample1)
+        l2 = inspect_notfound(sample2)
+        #print l
+        o="0"
+        self.assertTrue(l1==o)
+        self.assertTrue(l2==o)
+    
+        
 if __name__ == "__main__" : 
     main()
     
